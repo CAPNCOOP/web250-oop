a396697d93e8933add8c1d2dce6e17f5daf000e9
Taylor W Cooper <t.cooper1412@gmail.com>
2024-09-16 13:48:03 -0400

    Finished 6.2 Constructor Arguments

--- asgn04-constructors/constructor.php
+++ asgn04-constructors/constructor.php
@@ -5,18 +5,26 @@ class bird
   public $commonName;
   public $latinName;
 
-  public function __construct($commonName, $latinName)
+  public function __construct($args = [])
   {
-    $this->commonName = $commonName;
-    $this->latinName = $latinName;
+    $this->commonName = $args['commonName'] ?? NULL;
+    $this->latinName = $args['latinName'] ?? NULL;
   }
 }
 
-$bird1 = new Bird('American Robin', 'Turdus Migratorius');
-$bird2 = new Bird('Eastern Towhee', 'Pipilo Erythrophthalmus');
+$bird1 = new Bird(['commonName' => 'American Robin', 'latinName' => 'Turdus Migratorius']);
+$bird2 = new Bird(['commonName' => 'Eastern Towhee', 'latinName' => 'Pipilo Erythrophthalmus']);
+$bird3 = new Bird(['commonName' => 'Acadian Flycatcher', 'latinName' => 'Empidonax Virescens']);
+$bird4 = new Bird(['commonName' => 'Carolina Wren', 'latinName' => 'Thryothorus Ludovicianus']);
 
 echo "Common Name: " . $bird1->commonName . "<br>";
 echo "Latin Name: " . $bird1->latinName;
 echo "<hr>";
 echo "Common Name: " . $bird2->commonName . "<br>";
 echo "Latin Name: " . $bird2->latinName;
+echo "<hr>";
+echo "Common Name: " . $bird3->commonName . "<br>";
+echo "Latin Name: " . $bird3->latinName;
+echo "<hr>";
+echo "Common Name: " . $bird4->commonName . "<br>";
+echo "Latin Name: " . $bird4->latinName;
